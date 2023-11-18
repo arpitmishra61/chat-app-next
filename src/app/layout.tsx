0947aba0785d7bab23fa24ee94e6b2e2d24e9af9
Arpit Mishra <arpitmishra61@gmail.com>
2023-11-18 23:27:05 +0530

    cross org fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&family=Skranji:wght@400;700&display=swap"
           rel="stylesheet"
