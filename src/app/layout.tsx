482b5ceb1c65f46d253767bdc20942269a39b85a
Arpit Mishra <arpitmishra61@gmail.com>
2023-11-19 16:26:48 +0530

    added interactive hieght to fit 100vh mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
